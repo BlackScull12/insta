c5c3089e9044540bb455175f09c4faed8be4ee19
BlackScull12 <87164998+BlackScull12@users.noreply.github.com>
2026-03-12 10:16:00 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,40 +1,25 @@
-// firebase.js
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-import { 
-getAuth,
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
-onAuthStateChanged,
-signOut
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-import {
-getFirestore,
-collection,
-addDoc,
-getDocs,
-onSnapshot,
-query,
-orderBy
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-import {
-getStorage,
-ref,
-uploadBytes,
-getDownloadURL
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+import { getStorage } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 
 const firebaseConfig = {
-apiKey: "AIzaSyAxt94UyMn8AP8PFaSHPJ29JnZQ2KI3kZw",
-authDomain: "chatgithub-e838d.firebaseapp.com",
-projectId: "chatgithub-e838d",
-storageBucket: "chatgithub-e838d.firebasestorage.app",
-messagingSenderId: "755589384017",
-appId: "1:755589384017:web:6af4c6d223d646cf36f570"
+
+apiKey:"PASTE",
+authDomain:"PASTE",
+projectId:"PASTE",
+storageBucket:"PASTE",
+messagingSenderId:"PASTE",
+appId:"PASTE"
+
 };
 
 const app = initializeApp(firebaseConfig);
