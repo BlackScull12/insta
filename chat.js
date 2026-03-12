477d9867c8574374a9c269aaa96a2fcc2d16c5fd
BlackScull12 <87164998+BlackScull12@users.noreply.github.com>
2026-03-12 10:23:04 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,4 +1,4 @@
-import {auth,db} from './firebase.js'
+import {db,auth} from "./firebase.js"
 
 import {
 collection,
@@ -9,40 +9,37 @@ onSnapshot
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 
-window.send = async ()=>{
+window.sendMessage = async()=>{
 
-const text=document.getElementById("msg").value
+let text=document.getElementById("msg").value
 
 await addDoc(collection(db,"messages"),{
 
+user:auth.currentUser.uid,
 text:text,
-user:auth.currentUser.email,
 time:Date.now()
 
 })
 
-document.getElementById("msg").value=""
-
 }
-
 
 const q=query(collection(db,"messages"),orderBy("time"))
 
 onSnapshot(q,(snap)=>{
 
-const box=document.getElementById("messages")
+let box=document.getElementById("chat")
 
 box.innerHTML=""
 
 snap.forEach(doc=>{
 
-const m=doc.data()
+let m=doc.data()
 
 box.innerHTML+=`
 
-<div class="message">
+<div class="msg">
 
-<b>${m.user}</b> : ${m.text}
+${m.text}
 
 </div>
 
