b21582bb7bd72283c7dcec2fb41d01f01a099dc9
BlackScull12 <87164998+BlackScull12@users.noreply.github.com>
2026-03-12 10:12:04 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,44 @@
+// firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { 
+getAuth,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+onAuthStateChanged,
+signOut
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import {
+getFirestore,
+collection,
+addDoc,
+getDocs,
+onSnapshot,
+query,
+orderBy
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+import {
+getStorage,
+ref,
+uploadBytes,
+getDownloadURL
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+
+const firebaseConfig = {
+apiKey: "AIzaSyAxt94UyMn8AP8PFaSHPJ29JnZQ2KI3kZw",
+authDomain: "chatgithub-e838d.firebaseapp.com",
+projectId: "chatgithub-e838d",
+storageBucket: "chatgithub-e838d.firebasestorage.app",
+messagingSenderId: "755589384017",
+appId: "1:755589384017:web:6af4c6d223d646cf36f570"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
